12bae4573d249ecfa1fe6880bbf9ab363f60e1a0
wolff <wolff@FARPADD2>
2014-05-28 07:39:16 +0200

    * reorg pt. I

--- src/de/tudresden/ias/eclipse/dlabpro/uasr/IPreferenceConstants.java
+++
@@ -1,7 +0,0 @@
-package de.tudresden.ias.eclipse.dlabpro.uasr;
-
-public interface IPreferenceConstants
-{
-  // JlabSpeechView
-  public static final String P_JSV_CONFIG = "dlabpro.demos.JlabSpeechView.config" ;
-}
